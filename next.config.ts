fa18d0e4ded4854fcee6ce99f35404ba0fc94f17
Fola <folaremidixon@gmail.com>
2025-06-03 09:09:45 +0100

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname),
+      "@components": path.resolve(__dirname, "components"),
+      "@components-custom": path.resolve(__dirname, "components-custom"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
