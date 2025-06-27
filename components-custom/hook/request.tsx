158a318bf971f538a697dd0218e99f434e24abe0
Fola <folaremidixon@gmail.com>
2025-06-27 20:02:04 +0100

    updating the password verification

--- components-custom/hook/request.tsx
+++ components-custom/hook/request.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 export const dynamic = "force-dynamic";
 
-/* eslint-disable @typescript-eslint/no-explicit-any */
+
 
 import { useState } from "react";
 
