4fe926948fd6878dda1e0e1768b83a5d953bf476
Fola <folaremidixon@gmail.com>
2026-01-22 05:18:06 +0100

    new update for jodex

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "JODEX Landlord Portal",
+  title: "Welcome to JODEX Landlord Portal",
   description: "Smart Renting Made Simple",
 };
 
